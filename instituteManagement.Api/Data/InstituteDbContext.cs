4a75f0bd13be9b12ebfba914e54a149473c4a867
Vinayaka <Vinayaka@DESKTOP-IKGGS43>
2024-10-02 11:09:34 +0530

    Add project files.

---
+++ instituteManagement.Api/Data/InstituteDbContext.cs
@@ -0,0 +1,14 @@
+﻿using instituteManagement.Api.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace instituteManagement.Api.Data
+{
+    public class InstituteDbContext:DbContext
+    {
+        public InstituteDbContext(DbContextOptions<InstituteDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+        DbSet<Student> Students { get; set; }
+    }
+}
